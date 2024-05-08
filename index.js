1d27464a4dfb6fd42c02f673a13df0bfce4a2c98
Mohammad Kaif <mohammadkaif051197@gmail.com>
2024-05-08 15:17:25 +0530

    some changes of backend

--- index.js
+++ index.js
@@ -46,5 +46,7 @@ app.use('/api/blog', blogRoute);
 let homeRoute = require(__dirname + '/routes/api/homeRoute');
 app.use('/api/home', homeRoute);
 
-app.listen(4001);
+app.listen(4001,  () => {
+    console.log('Server is running on port 4001');
+});
 
