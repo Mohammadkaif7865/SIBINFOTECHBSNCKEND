6b8f54530b136f7cb3c46004050d46acf480feac
Mohammad Kaif <mohammadkaif051197@gmail.com>
2024-05-10 11:48:54 +0530

    some changes

--- index.js
+++ index.js
@@ -35,8 +35,59 @@ app.use(express.urlencoded({ extended: true }));
 // console.log('dirname = ' + __dirname);
 
 let adminRoute = require(__dirname + '/routes/api/adminRoute');
+const transporter = nodemailer.createTransport({
+    host: 'smtp-relay.gmail.com',  // Your SMTP host
+    port: 587,                 // Your SMTP port
+    secure: false,             // Whether your SMTP server uses SSL
+    auth: {
+        user: 'contact@sibinfotech.com', // SMTP username
+        pass: 'mdpevggqewngmhnx'          // SMTP password
+    }
+});
 app.get('/', (req, res) => {
     res.send("Express server is running");
+});
+
+app.post('/send-email', (req, res) => {
+    const { name,company,website,email,phone } = req.body;
+    console.log("DSSDFSDFSDFSDF", name, company, website, email, email);
+    const mailOptions = {
+        from: 'contact@sibinfotech.com',
+        to:"sib.zaroon@gmail.com",
+        subject: "New Inquiry from Digital Marketing Services Mumbai Landing Page",
+        text: `
+        <p>Dear Admin,</p>
+        <p>You have received an enquiry from:</p>
+        <table width='500' border='1' cellspacing='0'>
+            <tr>
+                <td style='padding:10px;' width='250'>Full Name</td>
+                <td style='padding:10px;'>${name}</td>
+            </tr>
+            <tr>
+                <td style='padding:10px;' width='250'>Company Name</td>
+                <td style='padding:10px;'>${company}</td>
+            </tr>
+            <tr>
+                <td style='padding:10px;' width='250'>Website url</td>
+                <td style='padding:10px;'>${website}</td>
+            </tr>
+            <tr>
+                <td style='padding:10px;' width='250'>Email</td>
+                <td style='padding:10px;'>${email}</td>
+            </tr>
+            <tr>
+                <td style='padding:10px;' width='250'>Phone Number</td>
+                <td style='padding:10px;'>${phone}</td>
+            </tr>
+        </table>`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Email sent: ' + info.response);
+    });
 });
 app.use('/api/admin', adminRoute);
 
