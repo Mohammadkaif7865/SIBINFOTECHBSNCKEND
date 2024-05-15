a77c6adfc75472a5c38292a015b087fad3d9abda
mohammad kaif <mohammadkaig051197@gmail.com>
2024-05-15 15:50:38 +0530

    some changes

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ const app = express();
 app.use(cors());
 
 // app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
+app.use(bodyParser.json());
 
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
