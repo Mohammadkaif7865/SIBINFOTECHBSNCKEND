08e2c3d89e509c34259ce8ae6af8e0517d5e6048
mohammad kaif <mohammadkaig051197@gmail.com>
2024-05-15 14:12:37 +0530

    sp,e chnages

--- index.js
+++ index.js
@@ -29,7 +29,95 @@ app.use(paginate.middleware(10, 50));
 app.get('/api', (req, res) => {
     res.status(200).json({ message: "Welcome to the API" });
 });
-
+app.post("/api/send-email", (req, res) => {
+    const { name, company, website, email, phone } = req.body;
+    console.log("DSSDFSDFSDFSDF", name, company, website, email);
+    const mailOptions = {
+      from: "SIB Infotech <contact@sibinfotech.com>",
+      to: "mohammadkaif051197@gmail.com",
+      // to: "contact@sibinfotech.com",
+      // cc: "radhey@sibinfotech.com",
+      subject: "New Inquiry from Digital Marketing Services Mumbai Landing Page",
+      html: `
+              <p>Dear Admin,</p>
+              <p>You have received an enquiry from:</p>
+              <table width='500' border='1' cellspacing='0'>
+                  <tr>
+                      <td style='padding:10px;' width='250'>Full Name</td>
+                      <td style='padding:10px;'>${name}</td>
+                  </tr>
+                  <tr>
+                      <td style='padding:10px;' width='250'>Company Name</td>
+                      <td style='padding:10px;'>${company}</td>
+                  </tr>
+                  <tr>
+                      <td style='padding:10px;' width='250'>Website url</td>
+                      <td style='padding:10px;'>${website}</td>
+                  </tr>
+                  <tr>
+                      <td style='padding:10px;' width='250'>Email</td>
+                      <td style='padding:10px;'>${email}</td>
+                  </tr>
+                  <tr>
+                      <td style='padding:10px;' width='250'>Phone Number</td>
+                      <td style='padding:10px;'>${phone}</td>
+                  </tr>
+              </table>`,
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        // Return an HTML page with a script for delayed redirection
+        res.send(`
+                  <html>
+                      <head><title>Submission Failed</title></head>
+                      <body>
+                          <h1>Failed to process your request</h1>
+                          <p>We encountered an error. You will be redirected shortly.</p>
+                          <script>
+                              setTimeout(function() {
+                                  window.location.href = 'https://sibinfotech.com/thanks';
+                              }, 5000); // Change 5000 to however many milliseconds you want
+                          </script>
+                      </body>
+                  </html>
+              `);
+      }
+      res.redirect("https://sibinfotech.com/thanks");
+    });
+  });
+  app.post("/api/send-email-any", (req, res) => {
+    const { html } = req.body;
+    console.log("DSSDFSDFSDFSDF", html);
+    const mailOptions = {
+      from: "SIB Infotech <contact@sibinfotech.com>",
+      to: "mohammadkaif051197@gmail.com",
+      subject: "New Inquiry from Digital Marketing Services Mumbai Landing Page",
+      html: html,
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        // Return an HTML page with a script for delayed redirection
+        res.send(`
+                  <html>
+                      <head><title>Submission Failed</title></head>
+                      <body>
+                          <h1>Failed to process your request</h1>
+                          <p>We encountered an error. You will be redirected shortly.</p>
+                          <script>
+                              setTimeout(function() {
+                                  window.location.href = 'https://sibinfotech.com/thanks';
+                              }, 5000); // Change 5000 to however many milliseconds you want
+                          </script>
+                      </body>
+                  </html>
+              `);
+      }
+      res.status(200).json({ message: "Email sent successfully" });  });
+  });
 app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.use('/public', express.static('assets'));
