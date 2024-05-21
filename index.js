e3ac22860e808020d6e6428aae0976205ce2f23b
mohammad kaif <mohammadkaig051197@gmail.com>
2024-05-21 16:14:59 +0530

    some api changes

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post("/api/send-email", (req, res) => {
       to: "contact@sibinfotech.com",
     //   to: "mohammadkaif051197@gmail.com",
       cc: "radhey@sibinfotech.com",
-      subject: `New Inquiry from Digital Marketing Services ${fromWhere} Landing Page`,
+      subject: `Digital Marketing Services Mumbai ${fromWhere} Landing Page`,
       html: `
               <p>Dear Admin,</p>
               <p>You have received an enquiry from:</p>
