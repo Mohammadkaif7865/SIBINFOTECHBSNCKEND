6c26382fc5d3b320a4c9a5edcedcea4f4566a2b3
sibinfotechgit <sibinfotech101@gmail.com>
2025-05-03 16:45:30 +0530

    corrected blog file upload issue

--- routes/api/blogRoute.js
+++ routes/api/blogRoute.js
@@ -3,7 +3,9 @@ var blogController = require("../../controllers/api/blogController");
 var router = express.Router();
 
 const path = require('path');
+
 let multer = require('multer');
+
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, 'uploads/blog/');
@@ -12,21 +14,27 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
+
 var upload = multer({ storage: storage })
+const cpUpload = upload.any(); // Accept any number of files with any field name
 
 var token = require('../../middlewares/token');
 
 router.get('/all', token.validateToken, blogController.blogsData);
 
-router.post('/add', token.validateToken, upload.fields([{
- name: 'image', maxCount: 1
-}]), blogController.blogAdd);
+// router.post('/add', token.validateToken, upload.fields([{
+//  name: 'image', maxCount: 1
+// }]), blogController.blogAdd);
+
+router.post('/add', token.validateToken, cpUpload, blogController.blogAdd);
 
 router.get('/single/:id', token.validateToken, blogController.blogEditData);
 
-router.post('/edit', token.validateToken, upload.fields([{
- name: 'image', maxCount: 1
-}]), blogController.blogEdit);
+// router.post('/edit', token.validateToken, upload.fields([{
+//  name: 'image', maxCount: 1
+// }]), blogController.blogEdit);
+
+router.post('/edit', token.validateToken, cpUpload, blogController.blogEdit);
 
 router.post('/delete', token.validateToken, blogController.blogDeleteData);
 
