99af47f3cf65cc63c21a5ad0500950a5d115d4f2
mohammad kaif <mohammadkaig051197@gmail.com>
2025-05-05 10:37:08 +0530

    some change

--- index.js
+++ index.js
@@ -148,7 +148,8 @@ app.post("/api/send-email-any", (req, res) => {
 
   const mailOptions = {
     from: "SIB Infotech <website@sibinfotech.com>",
-    to: "contact@sibinfotech.com",
+    // to: "contact@sibinfotech.com",
+    to: "sibinfotech101@gmail.com",
     cc: "radhey@sibinfotech.com",
     subject: `${fromWhere}`,
     html: html,
